ce170f2688feff6bc414319ffc4f9fd2a908a894
josuerx12 <josueaze12@gmail.com>
2023-07-22 17:44:38 -0300

    styles update

--- src/components/SummaryCart/SummaryCart.jsx
+++ src/components/SummaryCart/SummaryCart.jsx
@@ -1,27 +1,29 @@
 import { cartDB } from "../../context/CartContext";
 import { Link } from "react-router-dom";
 import currencyFormat from "../../utils/currencyFormat";
-import "./SummaryCart.sass";
 import { FaWhatsapp } from "react-icons/fa";
 import Orquestra from "../../assets/icons/Orquestra.ico";
+import "./SummaryCart.sass";
 
 const SumaryCart = () => {
   const { cart } = cartDB();
   const total = cart.reduce((acc, product) => acc + Number(product.total), 0);
 
   return (
-    <div>
+    <div className="summary">
       <h3>Resumo:</h3>
       <p>
-        <span>Valor dos produtos: </span> {currencyFormat(total)}
+        <span>Valor total dos produtos: </span> {currencyFormat(total)}
       </p>
-      <Link to="/" target="_blank">
-        Comprar via Orquestra
-        <img src={Orquestra} alt="orquestra icon" />
-      </Link>
-      <Link to="/">
-        Duvidas pelo <FaWhatsapp />
-      </Link>
+      <div className="Links">
+        <Link to="/" target="_blank" className="orquestra">
+          Comprar via Orquestra
+          <img src={Orquestra} alt="orquestra icon" />
+        </Link>
+        <Link to="/" target="_blank" className="whatsapp">
+          Duvidas pelo whatsapp <FaWhatsapp />
+        </Link>
+      </div>
     </div>
   );
 };
